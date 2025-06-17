bb112e988db5661340def78c9d7c2a0f9e769aac
michaeldebetaz <netarion@outlook.com>
2025-06-17 19:20:50 +0200

    feat: finished CH2 Functions L4 Write File

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,13 +6,10 @@ def get_files_info(working_directory: str, directory: str | None = None) -> str:
     if not working_directory_path.is_dir():
         return f"Error: {working_directory} is not a valid directory"
 
-    path = working_directory_path.resolve()
+    path = working_directory_path
 
     if directory is not None:
-        directory_path = Path(directory)
-        path = path / directory_path
-        path = path.resolve()
-
+        path = (path / Path(directory)).resolve()
         if path != working_directory_path:
             if working_directory_path not in path.parents:
                 return f'Error: Cannot list "{path}" as it is outside the permitted working directory'
@@ -26,3 +23,48 @@ def get_files_info(working_directory: str, directory: str | None = None) -> str:
         lines.append(line)
 
     return "\n".join(lines)
+
+
+def get_file_content(working_directory: str, file_path: str) -> str:
+    working_directory_path = Path(working_directory).resolve()
+    if not working_directory_path.is_dir():
+        return f"Error: {working_directory} is not a valid directory"
+
+    path = (working_directory_path / Path(file_path)).resolve()
+
+    if path != working_directory_path and working_directory_path not in path.parents:
+        return f'Error: Cannot read "{path}" as it is outside the permitted working directory'
+
+    if not path.is_file():
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            MAX_CHARS = 10_000
+            content = file.read(MAX_CHARS)
+            if len(content) == MAX_CHARS:
+                content += f'\n[...File "{path}" truncated at 10000 characters]'
+
+            return content
+    except Exception as e:
+        return f"Error reading file: {e}"
+
+
+def write_file(working_directory: str, file_path: str, content: str) -> str:
+    working_directory_path = Path(working_directory).resolve()
+    if not working_directory_path.is_dir():
+        return f"Error: {working_directory} is not a valid directory"
+
+    path = (working_directory_path / Path(file_path)).resolve()
+    if path != working_directory_path and working_directory_path not in path.parents:
+        return f'Error: Cannot write to "{path}" as it is outside the permitted working directory'
+
+    if not path.is_file():
+        return f'Error: Cannot write to "{path}" as it is not a regular file'
+
+    try:
+        with open(path, "w", encoding="utf-8") as file:
+            file.write(content)
+            return f'Successfully write to"{path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Error writing file: {e}"
--- tests.py
+++ tests.py
@@ -1,23 +1,63 @@
 import unittest
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_files_info, get_file_content, write_file
+
+WORKING_DIRECTORY = "calculator"
 
 
 class TestGetFilesInfo(unittest.TestCase):
-    def has_error(self, directory: str) -> bool:
-        working_directory = "calculator"
-        result = get_files_info(working_directory, directory)
-        print(result)
-        return result.startswith("Error: ")
+    # def test_get_files_info(self):
+    #     def check(directory: str) -> tuple[bool, str]:
+    #         result = get_files_info(WORKING_DIRECTORY, directory)
+    #         has_error = result.startswith("Error")
+    #         return has_error, result
+    #
+    #     # Test with valid directories
+    #     has_error, result = check(".")
+    #     self.assertFalse(has_error, "Expected no error for '.', got:\n" + result)
+    #     has_error, result = check("pkg")
+    #     self.assertFalse(has_error, "Expected no error for 'pkg', got:\n" + result)
+    #
+    #     # Test with invalid directories
+    #     has_error, result = check("/bin")
+    #     self.assertTrue(has_error, "Expected an error for '/bin', got:\n" + result)
+    #     has_error, result = check("../")
+    #     self.assertTrue(has_error, "Expected an error for '../', got:\n" + result)
+    #
+    # def test_get_file_content(self):
+    #     def check(file_path: str) -> tuple[bool, str]:
+    #         result = get_file_content(WORKING_DIRECTORY, file_path)
+    #         has_error = result.startswith("Error")
+    #         return has_error, result
+    #
+    #     # Test with valid files
+    #     has_error, result = check("main.py")
+    #     self.assertFalse(has_error, "Expected no error for 'main.py', got:\n" + result)
+    #     has_error, result = check("pkg/calculator.py")
+    #
+    #     # Test with invalid files
+    #     has_error, result = check("/bin/cat")
+    #     self.assertTrue(has_error, "Expected an error for '/bin/cat', got:\n" + result)
 
-    def test_get_files_info(self):
-        self.assertFalse(
-            self.has_error("."), "Expected no error for current directory '.'"
-        )
-        self.assertFalse(self.has_error("pkg"), "Expected no error for directory 'pkg'")
-        self.assertTrue(
-            self.has_error("/bin"), "Expected an error for directory '/bin'"
-        )
-        self.assertTrue(self.has_error("../"), "Expected an error for directory '../'")
+    def test_write_file(self):
+        def check(file_path: str, content: str) -> tuple[bool, str]:
+            result = write_file(WORKING_DIRECTORY, file_path, content)
+            print(result)
+            has_error = result.startswith("Error")
+
+            expected = "an error" if has_error else "no error"
+            message = f"Expected {expected} for writing to {file_path} {result=}"
+
+            return has_error, message
+
+        # Test writing to a valid file
+        has_error, msg = check("lorem.txt", "wait, this isn't lorem ipsum")
+        self.assertFalse(has_error, msg)
+        has_error, msg = check("pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+        self.assertFalse(has_error, msg)
+
+        # Test writing to an invalid file
+        has_error, msg = check("/tmp/temp.txt", "this should not be allowed")
+        self.assertTrue(has_error, msg)
 
 
 if __name__ == "__main__":
